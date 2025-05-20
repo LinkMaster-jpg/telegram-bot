9394a2dbc94c0d01854ef28645c18be37e89d941
LinkMaster-jpg <chainstore.ct.ws@gmail.com>
2025-05-20 21:45:27 +0300

    Add files via upload

---
+++ scrape_temuproducts.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_temuproduct_links():
+    url = "https://temu.to/k/e6xiugmdprq"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    links = []
+
+    for a in soup.find_all("a", href=True):
+        href = a["href"]
+        if "/goods.html?" in href and href.startswith("http"):
+            if href not in links:
+                links.append(href)
+
+    return links
